468db48fc5cec31e063204f4092e3c382349de1b
gigix1980 <gigix1980@0b35052c-5c29-0410-973e-e1fad6fcadea>
2007-02-14 01:33:35 +0000

    initial commit

---
+++ trunk/blog_to_book.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+
+require File.dirname(__FILE__) + "/" + "blog_entries.rb"
+require File.dirname(__FILE__) + "/" + "blog_entry.rb"
+
+src_dir = "TransparentThoughts"
+src_dir = "test-src"
+BlogEntries.new(src_dir).to_html
